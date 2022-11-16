3994e26cec43d45a65c37f4222c8088acc269f6e
Md Tohfatul Islam <tohfatul.iota@gmail.com>
2022-11-15 21:46:58 -0500

    updates

--- Assignment4/app/src/main/java/com/example/assignment4/MainActivity.kt
+++ Assignment4/app/src/main/java/com/example/assignment4/MainActivity.kt
@@ -2,6 +2,8 @@ package com.example.assignment4
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_main.*
 import layout.User
 
 class MainActivity : AppCompatActivity() {
@@ -18,5 +20,21 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        btn_login.setOnClickListener{
+            var username = txtEmail.text.toString()
+            var passwordd = txtPassword.text.toString()
+            for (user in users)
+            {
+                if(username == user.username && passwordd == user.password)
+                {
+                    Toast.makeText(this, "Great", Toast.LENGTH_SHORT).show()
+                }
+                else
+                {
+                    Toast.makeText(this, "Failed", Toast.LENGTH_SHORT).show()
+                }
+            }
+        }
     }
 }
